3e96cf9ef34f20e33967d8bc9f421ef3daccfc7d
Roman <roma_tym@mail.ru>
2018-09-24 00:13:39 +0300

    part 1

--- game.js
+++ game.js
@@ -52,48 +52,301 @@ class Actor {
         } else {
             this.speed = speed;
         }
+
+        Object.defineProperty(this, 'left', {
+            //writable: false,
+            get: function () {
+                return this.pos.x;
+            }
+        });
+        Object.defineProperty(this, 'right', {
+            //writable: false,
+            get: function () {
+                return this.pos.x + this.size.x
+            }
+        });
+        Object.defineProperty(this, 'top', {
+            //writable: false,
+            get: function () {
+                return this.pos.y  + this.size.y
+            }
+        });
+        Object.defineProperty(this, 'bottom', {
+            //writable: false,
+            get: function () {
+                return this.pos.y
+            }
+        });
+        Object.defineProperty(this, 'type', {
+            //writable: false,
+            get: function () {
+                return this;
+            }
+        });
     }
 
-    act() {
+    isIntersect(otherActor) {
+            if(!otherActor instanceof Actor) {
+                throw 'Неправильный тип объекта otherActor';
+            }
+            if(otherActor === undefined) {
+                throw 'Не задан тип объекта otherActor';
+            }
+            if(otherActor === this) {
+                return false;
+            }
+            if(
+                (this.left <= otherActor.right && otherActor.right <= this.right
+                || this.left <= otherActor.left && otherActor.left <= this.right)
+                &&
+                (this.bottom <= otherActor.top && otherActor.top <= this.top
+                ||  this.bottom <= otherActor.bottom && otherActor.bottom <= this.top)
+            ) {
+                return true
+            } else return false;
+        }
+}
 
+// const items = new Map();
+// const player = new Actor();
+// items.set('Игрок', player);
+// items.set('Первая монета', new Actor(new Vector(10, 10)));
+// items.set('Вторая монета', new Actor(new Vector(15, 5)));
+//
+// function position(item) {
+//     return ['left', 'top', 'right', 'bottom']
+//         .map(side => `${side}: ${item[side]}`)
+//         .join(', ');
+// }
+//
+// function movePlayer(x, y) {
+//     player.pos = player.pos.plus(new Vector(x, y));
+// }
+//
+// function status(item, title) {
+//     console.log(`${title}: ${position(item)}`);
+//     if (player.isIntersect(item)) {
+//         console.log(`Игрок подобрал ${title}`);
+//     }
+// }
+//
+// items.forEach(status);
+// movePlayer(10, 10);
+// items.forEach(status);
+// movePlayer(5, -5);
+// items.forEach(status);
+
+class Level {
+    constructor(grid, actors) {
+        this.grid = grid;
+        this.actors = actors;
+        if(!actors === undefined) {
+            let player = actors.filter(actor => actor.type === actor);
+            this.player = player[0];
+        }
+        if(!grid === undefined) {
+            this.height = grid.length;
+            this.width = grid.reduce(function(max, current) {
+                return Math.max(max, current.length);
+            }, 0);
+        }
+        this.status = null;
+        this.finishDelay = 1;
     }
 
-    Object.defineProperty(this, 'left', {
-        writable: false,
-        value: generateId()
-    });
+    // get status() {
+    //     return ;
+    // }
 
+    isFinished() {
+        if(!status === null && finishDelay < 0 ) {
+            return true;
+        }
+        return false;
+    }
+    actorAt(actorCheck) {
+        if(!actorCheck instanceof Actor) {
+            throw 'Неправильный тип объекта Actor';
+        }
+        if(actorCheck === undefined) {
+            throw 'Не задан тип объекта Actor';
+        }
+        for (let item of this.actors) {
+            if (actorCheck.isIntersect(item)) {
+                return item;
+            }
+        }
+        return undefined;
+    }
+        obstacleAt(vectorAt, vectorSize) {
+        if(!vectorAt instanceof Vector) {
+            throw 'Неправильный тип объекта vectorAt';
+        }
+        if(!vectorSize instanceof Vector) {
+            throw 'Неправильный тип объекта vectorSize';
+        }
 
-    isIntersect() {
+        for(let counterX = vectorAt.x-1; ++counterX; counterX < vectorSize.x) {
+            for(let counterY = vectorAt.y-1; ++counterY; counterY < vectorSize.y) {
+                if(counterY < 0) {
+                    return 'lava';
+                } else if(counterY > this.grid.length) {
+                    return 'wall';
+                } else if(counterX > this.grid[counterY].length) {
+                    return 'wall';
+                } else if (this.grid[counterY][counterX] != undefined) {
+                    return this.grid[counterY][counterX];
+                }
+            }
+        }
+        return undefined;
+    }
+    removeActor(Actor) {
+        this.grid.forEach(function(row) {
+            row.forEach(function(cell) {
+                if(this.grid[row][cell] === Actor) {
+                    this.grid[row][cell] = undefined;
+                    return;
+                }
+            })
+        });
+    }
 
+    noMoreActors(actorType) {
+        this.grid.forEach(function(row, indexY, arrY) {
+            row.forEach(function(cell, indexX, arrX) {
+                if(cell != undefined && cell.type  === actorType) {
+                    return false;
+                }
+            })
+        });
+       return true;
+    }
+
+    playerTouched(objectType, objectActor) {
+        if(this.status === null ) {
+            return;
+        }
+        if(objectType === 'lava' || objectType === 'fireball') {
+            this.status = 'lost';
+            return;
+        }
+        if(objectType === 'coin') {
+            this.removeActor(objectActor);
+        }
+        if(this.noMoreActors('coin')) {
+            this.status = 'won';
+        }
     }
 }
 
-const items = new Map();
-const player = new Actor();
-items.set('Игрок', player);
-items.set('Первая монета', new Actor(new Vector(10, 10)));
-items.set('Вторая монета', new Actor(new Vector(15, 5)));
+// const grid = [
+//     [undefined, undefined],
+//     ['wall', 'wall']
+// ];
+//
+// function MyCoin(title) {
+//     this.type = 'coin';
+//     this.title = title;
+// }
+// MyCoin.prototype = Object.create(Actor);
+// MyCoin.constructor = MyCoin;
+//
+// const goldCoin = new MyCoin('Золото');
+// const bronzeCoin = new MyCoin('Бронза');
+// const player = new Actor();
+// const fireball = new Actor();
+//
+// const level = new Level(grid, [ goldCoin, bronzeCoin, player, fireball ]);
+//
+// level.playerTouched('coin', goldCoin);
+// level.playerTouched('coin', bronzeCoin);
+//
+// if (level.noMoreActors('coin')) {
+//     console.log('Все монеты собраны');
+//     console.log(`Статус игры: ${level.status}`);
+// }
+//
+// const obstacle = level.obstacleAt(new Vector(1, 1), player.size);
+// if (obstacle) {
+//     console.log(`На пути препятствие: ${obstacle}`);
+// }
+//
+// const otherActor = level.actorAt(player);
+// if (otherActor === fireball) {
+//     console.log('Пользователь столкнулся с шаровой молнией');
+// }
 
-function position(item) {
-    return ['left', 'top', 'right', 'bottom']
-        .map(side => `${side}: ${item[side]}`)
-        .join(', ');
-}
 
-function movePlayer(x, y) {
-    player.pos = player.pos.plus(new Vector(x, y));
-}
+class LevelParser extends Level {
+    constructor(symbolsVocabulary) {
+        //super();
+    }
+    actorFromSymbol(strActor) {
+        if(strActor === '@') return Actor.prototype.constructor;
+        else if(strActor === 'o') return Actor.prototype.constructor;
+        else if(strActor === '=') return Actor.prototype.constructor;
+        else if(strActor === '|') return Actor.prototype.constructor;
+        else if(strActor === 'v') return Actor.prototype.constructor;
+        else return undefined;
+    }
+    obstacleFromSymbol(strObstacle) {
+        if(strObstacle === 'x') return 'wall';
+        else if(strObstacle === '!') return 'lava';
+        else return undefined;
+    }
+    createGrid(strArray) {
+        let grid = [];
+        for(let row of strArray) {
+            let newRow = [];
+            for(let cell of row) {
+                newRow.push(this.obstacleFromSymbol(cell));
+            }
+            grid.push(newRow);
+        }
+        return grid;
+    }
+    createActors(strArray) {
+        let actors = [];
+        for(let row of strArray) {
+            //let newRow = [];
+            for(let cell of row) {
+                let prototypeConstructor = this.actorFromSymbol(cell);
+                if(prototypeConstructor === undefined) {
+                    //newRow.push(undefined);
+                } else {
+                    let newObj = new prototypeConstructor(new Vector(row.indexOf(cell), strArray.indexOf(row)));
+                    actors.push(newObj);
+                }
+            }
+            //actors.push(newRow);
+        }
+        return actors;
+    }
+    parse(strArray) {
+        let grid = this.createGrid(strArray);
+        let actors = this.createActors(strArray);
+        return new level(grid, actors);
+        //this.super.constructor; //
 
-function status(item, title) {
-    console.log(`${title}: ${position(item)}`);
-    if (player.isIntersect(item)) {
-        console.log(`Игрок подобрал ${title}`);
+        //return level1;
     }
 }
 
-items.forEach(status);
-movePlayer(10, 10);
-items.forEach(status);
-movePlayer(5, -5);
-items.forEach(status);
+const plan = [
+    ' @ ',
+    'x!x'
+];
+
+const actorsDict = Object.create(null);
+actorsDict['@'] = Actor;
+
+const parser = new LevelParser(actorsDict);
+//let a = parser.createActors(plan);
+const level = parser.parse(plan);
+
+level.grid.forEach((line, y) => {
+    line.forEach((cell, x) => console.log(`(${x}:${y}) ${cell}`));
+});
+
+level.actors.forEach(actor => console.log(`(${actor.pos.x}:${actor.pos.y}) ${actor.type}`));
